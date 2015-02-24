e71bbc93f4834954b31dfc05d85a38e060568132
math29 <maaath29@gmail.Com>
2015-02-24 22:44:19 +0100

    chooceLoad

--- menuBar.py
+++ menuBar.py
@@ -1,6 +1,7 @@
 from Tkinter import Tk, Frame, Menubutton, Menu
 
 import pickle
+from tkFileDialog import *
 
 class MenuBar(Frame):
 	def __init__(self, parent=None):
@@ -12,7 +13,8 @@ class MenuBar(Frame):
 
 		menuFile = Menu(mbuttonFile)
 		menuFile.add_command(label="Enregistrer Default", command=self.defaultSave)
-		menuFile.add_command(label="Ouvrir configuration Default", command=self.defaultLoad)
+		menuFile.add_command(label="Charger Default", command=self.defaultLoad)
+		menuFile.add_command(label="Charger Choix", command=self.load)
 		menuFile.add_command(label="Quitter sans Enregistrer", command=parent.quit)
 
 		mbuttonFile.configure(menu=menuFile)
@@ -37,6 +39,16 @@ class MenuBar(Frame):
 			self.parent.control_X.scale_P.set(parameters["phase"])
 			self.parent.control_time.scale_time.set(parameters["time"])
 			self.parent.control_X.scale_P.update_signal()
+
+	def load(self):
+		fichier = askopenfile(mode='rb')
+		mon_depickler = pickle.Unpickler(fichier)
+		parameters = mon_depickler.load()
+		self.parent.control_X.scale_A.set(parameters["amplitude"])
+		self.parent.control_X.scale_F.set(parameters["frequence"])
+		self.parent.control_X.scale_P.set(parameters["phase"])
+		self.parent.control_time.scale_time.set(parameters["time"])
+		self.parent.control_X.scale_P.update_signal()
 
 if __name__ == "__main__":
     root = Tk()
